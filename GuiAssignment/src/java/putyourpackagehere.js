f488822f2f83737670506dab82ac42fa69326951
senyang <senyangfoo@gmail.com>
2024-04-07 16:11:34 +0800

    added text

--- GuiAssignment/src/java/putyourpackagehere.js
+++ GuiAssignment/src/java/putyourpackagehere.js
@@ -1,6 +1,3 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
- */
+//after added package can delete me!!!
 
 
